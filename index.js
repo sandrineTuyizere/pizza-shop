7e01f6351dd6487e13697a5c79a99bb949a779d6
Sandrine Tuyizere <sandrauyizere@gmail.com>
2020-10-10 18:29:48 +0200

    update

---
+++ index.js
@@ -0,0 +1,102 @@
+$(document).ready(function() {
+    $(".contacts").hide();
+    $(".delivery").hide();
+});
+
+
+function runCode(){
+
+    if (handlePrice()>0){
+        alert("The total price to pay is "+handlePrice()+" Rwf")
+        $("#delivery").show();
+        $("#order").hide();
+        $(".menu").hide();
+
+        document.querySelector('#delivery').scrollIntoView({
+            behavior: 'smooth' 
+        });
+    }
+    
+    function handlePrice(){
+        var a = document.getElementById("pizzaSize");
+        var pizzaSize = a.options[a.selectedIndex].value;
+
+        var b = document.getElementById("crustKind");
+        var crustKind = b.options[b.selectedIndex].value;
+
+        var c  = document.getElementById("toppings");
+        var toppingType = c.options[c.selectedIndex].value;
+
+
+        var price = 0;
+            if(pizzaSize === "small"){
+                price += 2000;
+            }
+            else if (pizzaSize === "medium"){
+                price += 3000;
+            }
+            else if(pizzaSize === "large"){
+                price += 5000;
+            }
+    
+    
+            if(crustKind === "crispy"){
+                price += 1000;
+            }
+            else if (crustKind === "stuffed"){
+                price += 1500;
+            }
+            else if (crustKind === "gluttenFree"){
+                price += 2000;
+            }
+            else if (crustKind=== "none"){
+                price +=0;
+            }
+            
+            if (document.getElementById("mushrooms").checked) {
+                price += 1000;
+                alert(price);
+            }
+            
+            if (document.getElementById("onions").checked) {
+                price += 500;
+            }
+
+            if (document.getElementById("pineapples").checked) {
+                price += 1000;
+            }
+            alert(price);
+            var numOfPizza = parseInt(document.getElementById("numOfPizza").value);
+            var totalPrice = price*numOfPizza;
+            return totalPrice;
+    }
+    
+   
+
+}
+
+function getDelivery(){
+    $(".delivery").hide();
+    $("#contacts").show();
+
+    document.querySelector('#contacts').scrollIntoView({
+        behavior: 'smooth' 
+    });
+
+}
+function generateMessage(){
+    var numOfPizza = parseInt(document.getElementById("numOfPizza").value);
+    var a = document.getElementById("pizzaSize");
+    var pizzaSize = a.options[a.selectedIndex].value;
+
+    var b = document.getElementById("crustKind");
+    var crustKind = b.options[b.selectedIndex].value;
+
+    var c  = document.getElementById("toppings");
+    var toppingType = c.options[c.selectedIndex].value;
+    var message = "You ordered "+numOfPizza+" "+ pizzaSize +" pizza with \n";
+    message += "a "+crustKind+" crust"+" and " + toppingType+ " toppings\n";
+    message += "\nDo you confirm your order?";
+    alert(message);
+} 
+
---
+++ readme.md
@@ -0,0 +1,58 @@
+# Pizza Shop
+The Pizza Shop for Tuyizere Sandrine (Created on 09 october2020)
+
+## Author
+Project by TUYIZERE Sandrine
+
+## Description
+this project is a part of Moringa School independent projects assessments. it is a website for Pizza Shop.
+a web application where your customers order different sizes of pizzas with one or more toppings. 
+
+
+## Technologies Used
+
+ * HTML
+ * Javascript
+ * CSS
+ * Jquerry
+
+## Behaviour
+
+### input
+ customers order different sizes of pizzas with one or more toppings. 
+### output
+checkout button that when clicked shows the total amount charged for the orders.
+## Setup and Installations
+
+### Prerequisites
+* internet connections
+* code editor (Visual studio code Recommended)
+* 
+* .
+
+### How to run the project
+use the command "git clone https://github.com/sandrineTuyizere/Portifolio.git in command line"
+
+## GitHub Pages link for the Project
+https://sandrinetuyizere.github.io/Portifolio/
+
+## support and contact details
+for more information,ideas or reccommendation you can reach to me by my email address : sadrauyizere@gmail.com
+
+## License
+
+MIT License
+
+Copyright (c) 2020 TUYIZERE Sandrine
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+
